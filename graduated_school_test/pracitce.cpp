e00e6187165a0303ca8a7e340eaf45dbdb532a0a
olccihyeon <sh102201@snu.ac.kr>
2021-10-13 20:08:09 +0900

    algorithm

---
+++ graduated_school_test/pracitce.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+	int arr[] = {9,8,7,6};
+    int a = 0;
+    cout<<arr[a]<<endl;
+    cout <<arr[a++]<<endl;
+    cout<<a<<endl;
+    cout<<arr[a++]<<endl;
+}
